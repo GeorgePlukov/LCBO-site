b9a963b0538a930e7a8e71d56b31831c81a0c784
GeorgePlukov <gplukov@gmail.com>
2015-05-30 12:02:43 -0400

    Test controller generated

---
+++ app/scripts/controllers/test.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name lcboSiteApp.controller:TestCtrl
+ * @description
+ * # TestCtrl
+ * Controller of the lcboSiteApp
+ */
+angular.module('lcboSiteApp')
+  .controller('TestCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
